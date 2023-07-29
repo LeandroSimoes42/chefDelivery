9bfe157557ed218d37b9746fc2da54748768073e
Leandro <98779007+LeandroSimoes42@users.noreply.github.com>
2023-07-29 16:02:15 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Lorenzo Campos on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
